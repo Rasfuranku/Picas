9b6a02b95ae1f403ccbc80d46b2bec126efc99f9
rasfuranku <marcelondonof@gmail.com>
2017-04-20 12:34:01 -0500

    baseAttemps

--- picas.js
+++ picas.js
@@ -1,28 +1,63 @@
-var getNumber = function(){
-
-}
 var generateRandomNumber = function(){
     var posibleNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
     var theNumber = [];
-    var round = 9;
-    var digit;
+    var round = 10;
     
-    // for(var i=0; i<4; i++){
-        var i = 0;
+    var i = 0;
     while(i < 4){
-        digit = Math.floor(Math.random() * (round - 0)) + 0;
-        var index = posibleNumbers.indexOf(digit);
+        var index = Math.floor(Math.random() * (round - 0)) + 0;
+        var digit = posibleNumbers[index];
 
-        if(index > -1){
-            theNumber.push(digit);
-            posibleNumbers.splice(index, 1);
-            i++;
-        }else{
-            digit = Math.floor(Math.random() * (round - 0)) + 0;
-        }
+        theNumber.push(digit);
+        posibleNumbers.splice(index, 1);
+
         round--;
+        i++
     }
 
-    console.log(theNumber);
     return theNumber;
+}
+
+var baseAttempts = function(){
+    var attemp = hits= 0;
+    var numberToGuest = generateRandomNumber();
+    var baseAtt = []; // 3 positions
+    console.log('numberToGuest: '+ numberToGuest);
+
+    while(attemp < 3){
+        baseAtt.push(generateRandomNumber());
+        console.log(baseAtt);
+        for(var i=0; i<baseAtt.length; i++){
+            var picas = fijas = 0;
+            for(var j=0; j<baseAtt[i].length; j++){
+                if(numberToGuest.indexOf(baseAtt[i][j]) == -1){
+                    console.log('0 picas, 0 fijas');
+                    picas = fijas = 0;
+                    hits += picas + fijas;
+                }else if(numberToGuest.indexOf(baseAtt[i][j]) == j){
+                    fijas++;
+                    hits += picas + fijas;
+                }else{
+                    picas++;
+                    hits += picas + fijas;
+                }
+                console.log("Picas:"+picas);
+                console.log("Fijas:"+fijas);
+            }
+        }
+        console.log('hits: '+hits);
+        if(hits == 4){
+            attemp = hits;
+        }if(hits == 0){
+            console.log("números eliminados");
+            console.log(baseAtt[i])
+            attemp++;
+        }else{
+            attemp++;
+        }
+    }
+}
+
+var checkPositions = function(){
+
 }
